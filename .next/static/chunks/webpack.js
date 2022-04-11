5cfae3492abf2074a3bcf3586442f45d5cd308e9
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-11 02:06:49 +0100

    token set

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b4ffb81bd12d6926"; }
+/******/ 		__webpack_require__.h = function() { return "113aa49f4f332f52"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- layout/Avatar.js
+++ layout/Avatar.js
@@ -4,11 +4,39 @@ import { ToastContainer,toast } from 'react-toastify'
 import { useRouter } from 'next/router';
 import 'react-toastify/dist/ReactToastify.css';
 import { profile } from '../api/authApi';
+import styled from "styled-components"
 
 export default function Avatar() {
+    const [avatars,setAvatars]=React.useState([]);
+    const [loading,setLoading]=useState(false);
+    const [selectedAvatar,setSelectedAvatar]=useState(undefined);    
+    const api="https://api.multiavatar.com/45678945";
+
+    const toastOption={
+        position: "bottom-right",
+        autoclose:8000,
+        pauseOnHover:true,
+        draggable:true,
+        theme:"dark"
+      }
+    const proflePicture=async()=>{
+
+    }
   return (
-    <div>
-        Avatar
-    </div>
+    <>
+        <Container>
+             <div className="title">
+                 <h1>Pick an Avatar as your profile picture</h1>
+             </div>
+             <div className="avatar">
+
+             </div>
+        </Container>
+        <ToastContainer limit={1}/>
+    </>
   )
 }
+
+const Container=styled.div`
+    
+`
