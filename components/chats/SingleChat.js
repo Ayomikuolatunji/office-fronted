0c24900e339214b78f8121e5cea73df5682c26a7
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-12 19:22:13 +0100

    single chat

--- components/chats/SingleChat.js
+++ components/chats/SingleChat.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import styled from "styled-components"
+export default function SingleChat({currentUser}) {
 
-export default function SingleChat() {
- const currenChat=useSelector(state=>state.users.chat)
- 
+
+  const currentChat=useSelector(state=>state.users.chat)
+  console.log(currentChat)
+
+
   return (
-    <div>SingleChat</div>
+    <>
+      <SingleChatDiv>
+          <div className="chat-header">
+              <div className="user-detaiils">
+                  <div className="avatar">
+                      
+                  </div>
+              </div>
+          </div>
+      </SingleChatDiv>
+    </>
   )
 }
+
+
+const SingleChatDiv=styled.div`
+
+
+`
