93f7e0c22ddb11e31ccadc7871902ddf8e29c4ce
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-13 02:43:17 +0100

    chat input form chat emoji

--- components/message-input/ChatInput.js
+++ components/message-input/ChatInput.js
@@ -2,21 +2,30 @@ import React, { useState } from 'react';
 import styled from 'styled-components'
 import {IoMdSend} from "react-icons/io"
 import {BsEmojiSmileFill} from "react-icons/bs"
+import InputEmoji from "react-input-emoji";
 
 
 export default function ChatInput({handlemessage}) {
-  const [chosenEmoji, setChosenEmoji] = useState(null);
-  
+  const [text, setText] = useState("");
+
+    const submit=(e)=>{
+      e.preventDefault()
+      console.log(text)
+    }
   return (
     <>
       <InputContainerStyle>
            <div className="button-container">
-               <div className="emoji">
+               {/* <div className="emoji">
                    <BsEmojiSmileFill/>
-               </div>
+               </div> */}
            </div>
-           <form className="form-input">
-             <input type="text" placeholder='message'/>
+           <form className="form-input" onSubmit={submit}>
+            <InputEmoji
+              value={text}
+              onChange={(e)=>setText(e.target.value)}
+              placeholder="Type a message"
+            />
              <button className='button'><IoMdSend/></button>
            </form>
       </InputContainerStyle>
@@ -29,11 +38,13 @@ const InputContainerStyle=styled.div`
       display: grid;
       grid-template-columns: 5% 95%;
       padding: 0.2rem;
-      background-color: #080420;
-      width: 90%;
+      background-color: #fff;
+      width: 95%;
       align-items: center;
       position: absolute;
       bottom: 0;
+      left: 0;
+      right: 0;
       margin: 30px;
       .button-container{
         display: flex;
@@ -51,12 +62,13 @@ const InputContainerStyle=styled.div`
       .form-input{
         width: 70%;
         border-radius: 2rem;
-        background-color: transparent;
-        background-color: green;
-
+        background-color: transparent;;
         input{
           width: 90%;
           height: 60px;
+          border: none;
+          text-indent: 10px;
+          font-size: 20px;
         }
       }
 `
