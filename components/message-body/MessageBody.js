fdd2bba1dabac2e40c72d6e3cc6f07bf64c50484
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-13 18:45:02 +0100

    chat message api call

--- components/message-body/MessageBody.js
+++ components/message-body/MessageBody.js
@@ -1,14 +1,24 @@
+import axios from 'axios'
 import React,{useEffect,useState} from 'react'
 import styled from "styled-components"
 import { fetchChats } from '../../api/chat-api'
+import { useSelector } from 'react-redux'
 
 export default function MessageBody() {
+  const {mainUser}=useSelector(state=>state.users.user)
+  const {contact}=useSelector(state=>state.users.chat)
     const [mgs,setMsg]=useState([])
 
 
     useEffect(()=>{
-           
-
+        async function fetchChat(){
+            const message=await axios.post(fetchChats,{
+              to: contact?._id,
+              from:mainUser?.user._id
+            })
+            console.log(message)
+        }
+        fetchChat()
     })
   
     return (
