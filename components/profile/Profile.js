5e9ed42e3df91acb0fbe33a2a40644a26dfb7443
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-17 00:33:26 +0100

    updating username

--- components/profile/Profile.js
+++ components/profile/Profile.js
@@ -35,7 +35,7 @@ export default function Profile() {
                 <ChangePicture currentUserImg={currentUserImg}/>
             </div>
             <div className="change-username">
-
+                 
             </div>
             <div className="change-password">
 
