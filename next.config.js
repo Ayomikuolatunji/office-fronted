b1efc319db95b77a907b0a1416557eedd84121e9
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-23 09:11:48 +0100

    fixed

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
     images: {
-    domains: ["college-sigunp-image.s3.amazonaws.com,college-img-preview.s3.eu-west-3.amazonaws.com","localhost"],
+    domains: ["college-img-preview.s3.eu-west-3.amazonaws.com"],
 },
 }
 
