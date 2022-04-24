2fe30a8ffcb5836eab64aa063ec228275b37820e
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-24 21:35:42 +0100

    bugs fixing in the entire app

--- util/modal/MuiModal.js
+++ util/modal/MuiModal.js
@@ -29,6 +29,7 @@ export default function MuiModal() {
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
+        
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
