ace289d9d50cda9b9e623281bb21c1b6124f0d4f
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-29 09:45:31 +0100

    frontend register company page

--- layout/RegisterCompany.js
+++ layout/RegisterCompany.js
@@ -3,13 +3,31 @@ import styled from "styled-components"
 
 export default function RegisterCompany() {
   return (
-    <div>RegisterCompany</div>
+    <div>
+      <div className="left-div">
+
+      </div>
+      <div className="right-div">
+
+      </div>
+    </div>
   )
 }
 
 
 const ContainerDiv=styled.div`
+    width:100%;
+    height: 100vh;
+    display: flex;
 
-
+      .left-div{
+        width: 50%;
+        display: block;
+        height: inherit;
+        background-image: url(/public/pexels-lisa-fotios-1957478.jpg);
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-size: cover;
+      }
 
 `
