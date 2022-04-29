14c73007c3965f301f80ac0f215fbcd74974ef9f
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-29 21:13:00 +0100

    list of companies

--- redux/companyTypeSlice.js
+++ redux/companyTypeSlice.js
@@ -1,6 +1,27 @@
-import {createSlice} from "@reduxjs/toolkit"
+import {createSlice,createAsyncThunk} from "@reduxjs/toolkit"
 
 const initialState={
     companies:[],
-    
+    selectedCompany:""
 }
+export const getCompanies=createAsyncThunk("companies/getCompanies",async()=>{
+    const res=await axios('localhost:8080/office-api/all-country-list')
+    return res.data
+})
+
+const companySlice=createSlice({
+    name:"companies",
+    initialState,
+    reducers:{
+        SelctedCompany:(state,action)=>{
+            state.selectedCompany=action.payload
+        }
+    },
+    extraReducers:{
+       [getCompanies.fulfilled]:(state,action)=>{
+
+       }
+    }
+})
+export const {SelctedCompany}=companySlice.actions
+export default companySlice.reducer
--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,10 @@
-import AllUserSlice from "./AllUser-slice";
 /* eslint-disable import/no-anonymous-default-export */
 import { configureStore,combineReducers} from '@reduxjs/toolkit'
 import { persistStore, persistReducer,} from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import thunk from 'redux-thunk'
 import countryApiSlice from "./countryApiSlice";
+import AllUserSlice from "./AllUser-slice";
 
 
 const persistConfig = {
