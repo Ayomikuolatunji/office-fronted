6549b3a735fb56b41f6e7031088ce7477af3befa
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-30 09:19:31 +0100

    register-company

--- layout/register-company/RegisterCompanyForm.js
+++ layout/register-company/RegisterCompanyForm.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React from 'react'
+import React,{useState} from 'react'
 import { useSelector } from 'react-redux'
 import { registerCompany } from '../../api/authApi'
 import { FormLogicHook } from '../../helpers/FormLogicHook'
@@ -10,12 +10,23 @@ import Button from "../../util/Button"
 
 export default function RegisterCompanyForm() {
  const  {values, handleChange}=FormLogicHook()
+ const [address,setAddress]=useState("")
  const country=useSelector(state=>state.country.seletedCountry)
- const handleSubmit=async()=>{
-     const res=await axios(registerCompany,{
+ const company=useSelector(state=>state.companies.companies)
+  const company_location=`${address}, ${country} `
+  console.log(company_location)
 
+ const handleSubmit=async(event)=>{
+     event.preventDefault()
+     const res=await axios(registerCompany,{
+          company_email:values.company_password,
+          company_location:company_location,
+          company_type:company,
+          company_name:values.company_name,
+          company_password:values.company_password
      })
- }     
+ } 
+
   return (
     <form className="right-div bg-gradient-to-r from-purple-500 to-pink-500 mx-a" onSubmit={(e)=>handleSubmit(e)}>
         <div className="text">
@@ -41,21 +52,22 @@ export default function RegisterCompanyForm() {
         </div>
         <div className="normal-input">
             <InputText 
-            text="Email"
-            name={values.company_name}
+            text="password"
+            name={values.company_password}
             onChange={(e)=>handleChange(e.target.value)}
             placeholder={"Enter your company account password"}
             />
         </div>
-          <div className='w-[70%] w-full flex mx-auto mf'>
+          <div className='w-[70%]  flex mx-auto mf'>
              <SelectCountry/>
              <SelectCompany/>
           </div>
           <div className="normal-input">
                 <textarea
                     text="text"
-                    name={values.company_address}
-                    onChange={(e)=>handleChange(e.target.value)}
+                    name={"addres"}
+                    value={address}
+                    onChange={(e)=>setAddress(e.target.value)}
                     placeholder={"Enter your company state, street and city"}
                     className="w-full p-5 bg-gray-100 hover:border-[1px] hover:border-[#997af0] rounded-[0.4rem]"
                 />
