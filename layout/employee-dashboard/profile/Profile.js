c032101da7fd05951eeba45192dc4f6ee46cf1ee
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-05 14:20:53 +0100

    employee dashboard

--- layout/employee-dashboard/profile/Profile.js
+++ layout/employee-dashboard/profile/Profile.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react'
 import { useSelector } from 'react-redux';
 import styled from "styled-components"
@@ -6,13 +7,21 @@ export default function Profile() {
   const employeeData=useSelector(state=>state.employeeInfo.employeeData)
   console.log(employeeData);
   return (
-    <div>Profile</div>
+    <ProfileContainer>
+        <div className="general w-[40%] p-2">
+            <Image src={employeeData.avartImage} width={200} height={200} alt={employeeData.username}/>
+        </div>
+         <div className="about">
+
+         </div>
+    </ProfileContainer>
   )
 }
 
 const ProfileContainer=styled.div`
-    
-
+    display: flex;
+    width: 100%;
+    height: inherit;
 `
 
 
