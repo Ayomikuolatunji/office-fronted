d569a309d3886984f132e13e1ba9adb0afb6ccd5
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-10 10:05:18 +0100

    profile p[icture update

---
+++ aws/upload.js
@@ -0,0 +1,18 @@
+export const upload=(file, link,setProgress,setUrl)=>{
+    setProgress("")
+    let xhr = new XMLHttpRequest();
+    // track upload progress
+    xhr.upload.onprogress = function(evt) {
+        setProgress(Math.round((evt.loaded / evt.total) * 100))
+    };
+    // track completion: both successful or not
+    xhr.onloadend = function() {
+      if (xhr.status == 200) {
+        setUrl(xhr.responseURL.split("?")[0]);
+      } else {
+        console.log("error " + this.status);
+      }
+    };
+    xhr.open("PUT", link);
+    xhr.send(file);
+  }
--- components/profile-components/PofilePicture.js
+++ components/profile-components/PofilePicture.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from "next/image"
 import {FcOldTimeCamera} from "react-icons/fc"
+import { upload } from '../../aws/upload'
 
 
 export default function ProfilePicture({employeeData}) {
+   const [url,seturl]=useState("")
+   const [progress,setProgress]=useState(0)
+  const graphQuery={
+    query:`
+    mutation {
+     update_Profile_Picture(id: "${employeeData._id}", update_picture: {avartImage: "${url}", avatarImageSet: true}) {
+       _id
+       username
+       email
+       avartImage
+     }
+   }  
+    `
+   }
+
+  const onImageChange=async(event) => {
+   const file=event.target.files[0]
+  try{
+      if (event.target.files && event.target.files[0]) {
+          const fileUpload=event.target.files[0]
+           const response = await axios({
+             method: 'GET',
+             headers:{
+               "Content-Type":"image/jpeg"
+             },
+                url:"https://115mf3u9df.execute-api.eu-west-3.amazonaws.com/default/collegeImgPreview"
+             })
+                 // PUT request: for upload url to S3
+             const result = await fetch(response.data.uploadURL, {
+                     method: 'PUT',
+                     body:fileUpload
+             })
+             upload(file,response.data.uploadURL,setProgress,seturl )
+            if(result.status===200){
+                 fetch("http://localhost:8080/graphql",{
+                   method:"POST",
+                   headers:{
+                     "Content-Type":"application/json"
+                   },
+                   body:JSON.stringify(graphQuery)
+                 })
+                 .then(js=>{
+                   return js.json()
+                 })
+                 .then(data=>{
+                   console.log(data)
+                 })
+                 .catch(err=>{
+                   console.log(err.message)
+                 })
+              }
+        }
+      }catch(error){
+        toast.error("uploading failed", toastOption);
+        return false 
+      }
+
+    }
   
   return (
     <div className='relative w-[200px] h-[200px] self-center'>
