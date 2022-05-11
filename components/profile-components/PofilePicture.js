fd9d8e5a887c3913961389db50d8e8e26a3817f6
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-11 14:39:50 +0100

    profile picture graphql fully integreted

--- components/profile-components/PofilePicture.js
+++ components/profile-components/PofilePicture.js
@@ -5,10 +5,11 @@ import {FcOldTimeCamera} from "react-icons/fc"
 
 
 export default function ProfilePicture({employeeData}) {
-
+  const [image, setImage] =React.useState("");
 
 
    const handleUpload = (uploadEvent) => {
+    setImage(URL.createObjectURL(uploadEvent.target.files[0]));
     uploadEvent.persist();
 
     const [file] = uploadEvent.target.files;
@@ -49,7 +50,7 @@ export default function ProfilePicture({employeeData}) {
   
   return (
     <div className='relative w-[200px] h-[200px] self-center'>
-    <Image src={employeeData.avartImage} alt={employeeData.username} layout='fill' className='rounded-[50%]'/>
+    <Image src={image ? image : employeeData.avartImage} alt={employeeData.username} layout='fill' className='rounded-[50%]'/>
     <label htmlFor="files" className='text-5xl absolute bottom-0 right-0 mr-3x]'>
       <FcOldTimeCamera className='text-6xl my-3 cursor-pointer'/>
       <input
