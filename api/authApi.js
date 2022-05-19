8d2a92ceb2063c0360886cea5aa713227b74c49b
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-19 22:15:04 +0100

    change empoyee register api

--- api/authApi.js
+++ api/authApi.js
@@ -1,12 +1,12 @@
 const host=`http://localhost:8080`
 
-export const registrationApi=`${host}/office-api/auth/register`
+export const registrationApi=`${host}/office-api/auth/register_employee`
 
 export const loginApi=`${host}/office-api/auth/login`
 
 export const profile=`${host}/office-api/auth/profile_picture`
 
-export const allUsers=`${host}/office-api/auth/all_users`
+export const allUsers=`${host}/office-api/auth/all_employees`
 
 export const deleteUserApi=`${host}/office-api/auth/delete_user`
 
