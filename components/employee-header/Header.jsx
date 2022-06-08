8ed9ae509e0381ecf176921a7165b4625aa2675f
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-08 20:54:37 +0100

    rebuilding employee dashboard

---
+++ components/employee-header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {GiHamburgerMenu} from "react-icons/gi";
+
+
+const Header = () => {
+  return (
+    <div className='w-[70%]'>
+        <div className='flex flex-row justify-between items-center'>
+             <div className="hamburger">
+
+             </div>
+             <div className="search">
+
+             </div>
+             <div className="add-company">
+                 
+             </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- layouts/EmplyeeDashhboard.js
+++ layouts/EmplyeeDashhboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../components/employee-header/Header'
 
 const EmplyeeDashhboard = () => {
   return (
     <div>
-      
+       <Header/>
     </div>
   )
 }
