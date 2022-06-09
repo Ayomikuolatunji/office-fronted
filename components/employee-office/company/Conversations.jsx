ec06e0932d8215ab66c5c6877d95ccc8d0d8170a
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-09 23:01:53 +0100

    I am current working with employee company data

--- components/employee-office/company/Conversations.jsx
+++ components/employee-office/company/Conversations.jsx
@@ -1,9 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
 const Conversations = () => {
   return (
     <div>
-        Conversations
+        <div className="intro">
+              <h4 className='text-gray-400 '>Conversations</h4>
+        </div>
+        <div className="chat-list-of-other-employees">
+            <div className="chat-list-of-other-employees-item">
+                <div className="chat-list-of-other-employees-item-avatar">
+                    <img 
+                    src="https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" 
+                    alt="img"
+                    className='w-[30px]'
+                    />
+               </div>
+                <div className="chat-list-of-other-employees-item-content">
+                    <div className="chat-list-of-other-employees-item-content-name">
+                        <h5>John Doe</h5>
+                        <p>Front-end developer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
