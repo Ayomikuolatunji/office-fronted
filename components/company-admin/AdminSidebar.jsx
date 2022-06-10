73e1646e0261bc01e92058c1243b5cd59ffc15cf
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-10 23:47:57 +0100

    clear

--- components/company-admin/AdminSidebar.jsx
+++ components/company-admin/AdminSidebar.jsx
@@ -23,10 +23,12 @@ const AdminSidebar = () => {
           </div>
             <ul className="space-y-2 py-4 px-3 ">
                <li>
-                  <a href="#" className="flex items-center p-2 text-base font-normal text-white rounded-lg dark:text-white hover:bg-gray-600 dark:hover:bg-gray-700">
-                     <svg className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
-                     <span className="ml-3">Dashboard</span>
-                  </a>
+                  <Link href={"/admin-dashboard"}>
+                     <a href="#" className="flex items-center p-2 text-base font-normal text-white rounded-lg dark:text-white hover:bg-gray-600 dark:hover:bg-gray-700">
+                        <svg className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
+                        <span className="ml-3">Dashboard</span>
+                     </a>
+                  </Link>
                </li>
                <li>
                {
@@ -43,7 +45,7 @@ const AdminSidebar = () => {
             }
                </li>
             </ul>
-            <ul className="pt-4 mt-4 space-y-2 border-t border-gray-200 dark:border-gray-700">
+            <ul className="pt-4 mt-5 space-y-2 border-t border-gray-200 dark:border-gray-700">
                <li>
                {
                   AdminSidebarSettings.map((menuItem, index) => {
