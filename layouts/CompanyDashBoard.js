430294917212873b3f9975b483124bed02109c03
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-10 21:32:57 +0100

    company admin dashboard under construction

--- layouts/CompanyDashBoard.js
+++ layouts/CompanyDashBoard.js
@@ -6,11 +6,11 @@ const CompanyDashBoard = ({children}) => {
 
   return (
     <div className='w-[100%] h-[100vh] font-serif'>
-         <div className="wrapper">
-             <div className="sidebar-wrapper">
+         <div className="wrapper w-[100%] h-[inherent] flex">
+             <div className="sidebar-wrapper w-[15%] h-[100vh]">
                  <AdminSidebar/>
              </div>
-             <main className="main">
+             <main className="main w-[75%]">
                 {children}
              </main>
          </div>
