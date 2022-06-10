e72408e671dc3dfaf7f34d4a14bc7be8c92bea7f
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-10 01:37:41 +0100

    clear

--- components/employee-office/company/CompanySidebar.jsx
+++ components/employee-office/company/CompanySidebar.jsx
@@ -8,7 +8,7 @@ import Conversations from './Conversations'
 const CompanySidebar = () => {
     const isCompanyOpen = useSelector(state => state.modal.isCompanyOpen)
 
-
+      
   return (
     <div className={`w-[100%] h-[100%] 
     duration-500 absolute transition-[margin-left] top-0 right-0 left-0 bottom-0 z-[999] ${isCompanyOpen ? "mr-0" :"ml-[100%]"}`}>
