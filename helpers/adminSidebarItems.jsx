9d70e05688396fe7083e5478268bb21a08035bea
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-10 22:17:50 +0100

    company admin dashboard under construction

--- helpers/adminSidebarItems.jsx
+++ helpers/adminSidebarItems.jsx
@@ -1,7 +1,20 @@
+import { AiOutlineTeam, AiTwotoneCalendar } from "react-icons/ai";
+import { GrProjects } from "react-icons/gr";
 
 
 export const adminSidebarItems=[
-  
-    
+  {
+    route:"/admin-dashboard/projects",
+    item:"Projects",
+    icon: <GrProjects/>
+  },{
+    route:"/admin-dashboard/employees",
+    item:"Employees",
+    icon: <AiOutlineTeam/>
+  },{
+      route:"/admin-dashboard/calendar",
+    item:"Calendar",
+    icon: <AiTwotoneCalendar/>
+  }
 
 ]
