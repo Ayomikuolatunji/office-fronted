65a3054509e7dd5b61f2b3c9c6487c43bfd737b7
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-11 01:03:52 +0100

    I am working on admin dashboard header

--- components/admin-header/Header.jsx
+++ components/admin-header/Header.jsx
@@ -3,20 +3,24 @@ import {BsSearch} from "react-icons/bs";
 
 const Header = () => {
   return (
-    <div>
+    <div className='w-[95%] mx-auto'>
         <div className='flex justify-between items-center'>
-             <div className="logo">
-                <h1>Onlineoffice Admin</h1>
+             <div className="logo w-[20%]">
+                <h1 className='text-lg'>Onlineoffice Admin</h1>
               </div>
-              <div className="search">
-                  
-                   <input type="text" 
-                    placeholder="Search members, projects, and more"  
+              {/* search */}
+              <div className="search space-x-5 relative mt-4 border-0 outline-none w-[70%]">
+                   <BsSearch
+                    className="text-[#c9c9c9] text-xl absolute left-10 top-3"
                    />
-              </div>
-              <div className="right">
+                    <input type="text" placeholder="Search projects, members and more" 
+                    className="w-full  p-2 mb-5 indent-14" 
+                    />
+                </div>
+                {/* end search */}
+              <div className="right w-[20%] flex justify-end">
                   <div className="user">
-                    <h3>Company name</h3>
+                    <h3 className='text-lg'>Company name</h3>
                   </div>
               </div>
         </div>
--- components/company-admin/tasks/Tasks.jsx
+++ components/company-admin/tasks/Tasks.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Tasks = () => {
   return (
-    <div>
+    <main className='tasks-body'>
         
-    </div>
+    </main>
   )
 }
 
