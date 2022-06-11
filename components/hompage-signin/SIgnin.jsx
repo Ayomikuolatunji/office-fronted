ea75d47b5bb5253c2734c03b6b9dccfec0a2220c
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-11 17:25:47 +0100

    clear

--- components/hompage-signin/SIgnin.jsx
+++ components/hompage-signin/SIgnin.jsx
@@ -6,11 +6,14 @@ import * as React from 'react';
 export default function Signin() {
 
   return (
-    <div>
-       <select name="login" id="" className={"rounded-lg mt-2 border-2 border-green-500  flex justify-between px-5 py-2 w-[150px] bg-black"}>
-           <option value="">Login</option>
+    <div className='mr-4'>
+       <select name="login" id="" className={"rounded-sm mt-2 border-2 border-[green] flex justify-between px-2 py-2 w-[150px] bg-black text-green-500"}>
+           <option value="default">Signin</option>
            <option value="login">
-                Login as employee
+             Login as employee
+           </option>
+           <option value="login">
+             Login as admin
            </option>
        </select>
     </div>
