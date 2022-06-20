2c442bc10a225184535e8d5f0db2085ae05dc422
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-20 21:00:45 +0100

    stylying register company page

--- components/company-authentications/register-company/index.js
+++ components/company-authentications/register-company/index.js
@@ -4,7 +4,7 @@ import RegisterCompanyForm from './RegisterCompanyForm'
 export default function RegisterCompany() {
   return (
     <div className='w-full h-[100vh] flex'>
-      <div className="left-div w-[50%] block bg-[url('/images/office.jpg')] bg-no-repeat bg-cover bg-fixed">
+      <div className="left-div w-[50%] block bg-red-500">
 
        </div>
       <RegisterCompanyForm/>
