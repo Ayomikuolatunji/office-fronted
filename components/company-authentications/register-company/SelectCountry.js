9fda77362d7c13288307b9268e1ca00860c89f8c
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-25 11:23:54 +0100

    fixing selection styles bugs

--- components/company-authentications/register-company/SelectCountry.js
+++ components/company-authentications/register-company/SelectCountry.js
@@ -1,50 +1,18 @@
 import React,{useEffect} from 'react';
-import { useTheme } from '@mui/material/styles';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+
 import { useDispatch, useSelector } from 'react-redux'
 import {fetchAllcountries} from "../../../redux/country/countryApiSlice"
 
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
-
-
-function getStyles(name, countryName, theme) {
-  return {
-    fontWeight:
-      countryName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  };
-}
 
 export default function SelectCountry() {
-  const theme = useTheme();
   const countries=useSelector(state=>state.country.countries) 
   const dispatch=useDispatch()
   const [countryName, setCountry] = React.useState([]);
 
   
   const handleChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-  
-    setCountry(
-      // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
-    );
+    setCountry(event.target.value);
   };
 
   useEffect(()=>{
@@ -54,39 +22,23 @@ export default function SelectCountry() {
 
   return (
     <div className='w-[100%] relative'>
-      <FormControl style={{width:"100%"}}>
-        <Select
-          multiple={false}
-          displayEmpty
+        <select
           value={countryName}
           onChange={handleChange}
-          input={<OutlinedInput />}
-          className="bg-gray-100 border-gray-200 border-[1px] 
-          w-[100%] relative"
-          renderValue={(selected) => {
-            if (selected.length === 0) {
-              return <em>Select country</em>;
-            }
-
-            return selected;
-          }}
-          MenuProps={MenuProps}
-          inputProps={{ 'aria-label': 'Without label' }}
+          className="w-full p-4 appearance-none bg-gray-50 border border-gray-300 text-gray-900 block"
         >
-          <MenuItem disabled value="">
+          <option disabled value="">
             <em>Provide your country </em>
-          </MenuItem>
+          </option>
           {countries.map((name) => (
             <option
               key={name.name}
               value={name.name}
-              style={getStyles(name.name, countryName, theme)}
             >
               {name.name}
             </option>
           ))}
-        </Select>
-      </FormControl>
+        </select>
     </div>
   );
 }
